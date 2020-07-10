112e687bf7aa64953f4790af4812e51ef8fe8950
Sam Eckmeier <eckmeier41@gmail.com>
2020-07-10 18:38:49 -0500

    adds donations association and after_create callback to payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,10 +4,24 @@ class Payment < ApplicationRecord
   self.implicit_order_column = 'created_at'
 
   belongs_to :non_profit
+  has_many :donations
 
-  validates :amount, presence: true, numericality: { greater_than: 0.00 }
+  validates :amount, presence: true, numericality: { greater_than: 0.0.to_d }
+
+  after_create :associate_donations
 
   def send_payment
     # TODOD: Implement deposit functionality to allow GiveLively to transfer funds from payments account to non-member non profit
+  end
+
+  private
+
+  def associate_donations
+    donations = non_profit.donations.unfulfilled
+
+    donations.update_all(payment_id: id, fulfilled: true, updated_at: DateTime.now)
+  rescue StandardError => e
+    logger.error(e)
+    raise ActiveRecord::Rollback
   end
 end
