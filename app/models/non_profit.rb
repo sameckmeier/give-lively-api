2f9aa4de5dc97b80f174ff4302c213f937a04d98
Sam Eckmeier <eckmeier41@gmail.com>
2020-07-10 18:38:06 -0500

    adds donations association to non_profit

--- app/models/non_profit.rb
+++ app/models/non_profit.rb
@@ -2,8 +2,9 @@
 
 class NonProfit < ApplicationRecord
   has_many :payments
+  has_many :donations
 
   validates :name, presence: true
 
-  scope :requires_payment, -> { where(member: true).where('unpaid_donation_amount > ?', 0.00) }
+  scope :requires_payment, -> { where(member: true).where('unpaid_donation_amount > ?', 0.0.to_d) }
 end
