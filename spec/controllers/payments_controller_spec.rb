6d883804d8a606d8843659ba613c18e91b30ae0d
Sam Eckmeier <eckmeier41@gmail.com>
2020-07-10 17:27:37 -0500

    implements payments controller create action

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -4,7 +4,28 @@ require 'rails_helper'
 
 describe Api::V1::PaymentsController do
   describe 'create' do
-    it 'creates record' do
+    context 'when params are invalid' do
+      it 'raises exception' do
+        post :create, params: { format: 'json' }
+
+        parsed_response = JSON.parse(response.body)
+
+        expect(parsed_response.key?('error')).to eq(true)
+      end
+    end
+
+    context 'when params are valid' do
+      it 'creates record' do
+        non_profit = NonProfit.create(name: 'Test1', address: 'test')
+
+        post :create, params: { payment: { non_profit_id: non_profit.id, amount: 1.0.to_d }, format: 'json' }
+
+        parsed_response = JSON.parse(response.body)['data']
+
+        expect(parsed_response.key?('id')).to eq(true)
+        expect(parsed_response['attributes']['non_profit_id']).to eq(non_profit.id)
+        expect(parsed_response['attributes']['amount']).to eq('1.0')
+      end
     end
   end
 end
