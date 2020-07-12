a9bd06dfb335397f0421f1cc797be344fab27a78
Sam Eckmeier <eckmeier41@gmail.com>
2020-07-12 18:23:47 -0500

    relpaces amount param with payment obj param for NonProfit#subtract_payment

--- app/models/non_profit.rb
+++ app/models/non_profit.rb
@@ -8,9 +8,11 @@ class NonProfit < ApplicationRecord
 
   scope :requires_payment, -> { where(member: false).where('unpaid_donation_amount > ?', 0.0.to_d) }
 
-  def subtract_payment(amount)
-    update!(unpaid_donation_amount: unpaid_donation_amount - amount.to_d)
-  rescue StandardError => e
-    logger.error("Failed to subtract donation amount #{amount.to_d} from Non Profit - ID #{id} - #{e.message}")
+  def subtract_payment(payment)
+    if payment.non_profit_id != id
+      raise 'Cannot subtract unassociated Payment amount from NonProfit'
+    end
+
+    update!(unpaid_donation_amount: unpaid_donation_amount - payment.amount)
   end
 end
